a6c674fb28f191baca9b64351064b295695c80b0
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2021-06-14 14:32:10 +0200

    feat(setup): initial setup

---
+++ iOS Twitter/iOS Twitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Twitter
+//
+//  Created by Jose Luis Franconetti Olmedo on 14/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Twitter/iOS Twitter/iOS_TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TwitterApp.swift
+//  iOS Twitter
+//
+//  Created by Jose Luis Franconetti Olmedo on 14/6/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
